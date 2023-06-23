255ed6ab66ea4e9ad420e9e1575efa01f43ed47f
Noor Ul Amin <noorawan444@gmail.com>
2023-06-23 11:12:25 +0500

    add version in apis (v1)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api/');
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
