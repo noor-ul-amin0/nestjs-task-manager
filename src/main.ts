64343e2db5bcbc898ba8aca0752345cc08cc7b1b
Noor Ul Amin <noorawan444@gmail.com>
2023-06-23 12:31:32 +0500

    configure swagger in app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('TodoList Application REST APIs')
+    .setDescription(
+      'A full-featured Todo List/Task Management application built with NestJS, PostgreSQL, and Sequelize, implementing authentication, user management, todo lists, tasks, and reporting functionalities.',
+    )
+    .setVersion('1.0')
+    .addTag('TodoList APIs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
