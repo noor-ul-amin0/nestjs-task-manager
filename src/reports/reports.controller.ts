94869a2d41a6f6f551bcee402101b892a2c877f2
Noor Ul Amin <noorawan444@gmail.com>
2023-06-23 12:32:45 +0500

    document reports module

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,27 +4,76 @@ import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/users.model';
 import { ITasksCount } from './reports.interfaces';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiForbiddenResponse,
+  ApiProperty,
+} from '@nestjs/swagger';
 
+class TasksCount {
+  @ApiProperty({ description: 'Total tasks' })
+  totalTasks: number;
+  @ApiProperty({ description: 'Completed tasks' })
+  completedTasks: number;
+  @ApiProperty({ description: 'Remaining tasks' })
+  remainingTasks: number;
+}
+
+@ApiTags('Reports')
+@ApiBearerAuth()
 @Controller('reports')
 @UseGuards(AuthGuard())
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+
   @Get('/task-counts')
+  @ApiOperation({ summary: 'Get total number of tasks for a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the total task count',
+    type: TasksCount,
+  })
+  @ApiForbiddenResponse({ description: 'Please add a Todo list first' })
   async getTaskCount(@GetUser() user: User): Promise<ITasksCount> {
     return this.reportsService.getTaskCount(user);
   }
 
   @Get('/avg-completed-per-day')
+  @ApiOperation({ summary: 'Get average tasks completed per day by a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the average tasks completed per day',
+    type: Number,
+  })
+  @ApiForbiddenResponse({ description: 'Please add a Todo list first' })
   getAvgTasksPerDay(@GetUser() user: User): Promise<number> {
     return this.reportsService.getAvgTasksPerDay(user);
   }
 
   @Get('/not-completed-on-time')
+  @ApiOperation({ summary: 'Get tasks not completed on time by a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the number of tasks not completed on time',
+    type: Number,
+  })
+  @ApiForbiddenResponse({ description: 'Please add a Todo list first' })
   getTasksNotCompletedOnTime(@GetUser() user: User): Promise<number> {
     return this.reportsService.getTasksNotCompletedOnTime(user);
   }
 
   @Get('/get-date-with-most-completed-tasks')
+  @ApiOperation({ summary: 'Get date with most tasks completed by a user' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Returns the date with most completed tasks or null if there are none',
+    type: Date,
+  })
+  @ApiForbiddenResponse({ description: 'Please add a Todo list first' })
   getDateWithMostCompletedTasks(@GetUser() user: User): Promise<Date | null> {
     return this.reportsService.getDateWithMostCompletedTasks(user);
   }
