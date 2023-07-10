793393beb48b7885fcdc1a1220db79c08dd9d669
Noor Ul Amin <noorawan444@gmail.com>
2023-07-10 19:22:12 +0500

    Throw error on extra fields in req body

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
