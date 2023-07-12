622e500133adcc6dc31fd6451de9169771dc83cc
Noor Ul Amin <noorawan444@gmail.com>
2023-07-12 16:58:19 +0500

    Add findAll route hanlder in users controller
    This route can only be accessed by Admin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,17 +4,32 @@ import {
   Delete,
   UseGuards,
   ParseIntPipe,
+  Get,
 } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { AuthGuard } from "@nestjs/passport";
 import { Roles } from "src/role/roles.decorator";
 import { Role } from "src/common/enums/role.enum";
 import { RolesGuard } from "src/common/guards/roles.guard";
+import { User } from "./users.model";
+import { plainToClass } from "class-transformer";
+import { UserEntity } from "src/entites/serialized-user.entity";
 
 @Controller("users")
 @UseGuards(AuthGuard("jwt"))
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
+  async findAll(): Promise<UserEntity[]> {
+    const users = await this.usersService.findAll();
+    return users.map((user: User) => {
+      const serializedUser = plainToClass(UserEntity, user);
+      return serializedUser;
+    });
+  }
 
   @Delete(":id")
   @Roles(Role.Admin)
