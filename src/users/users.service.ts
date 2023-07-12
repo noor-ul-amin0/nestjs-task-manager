2c18e863db9b25871b4b1c4daf0800e80e243350
Noor Ul Amin <noorawan444@gmail.com>
2023-07-12 16:57:17 +0500

    Add findAll users service method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { User } from './users.model';
-import { InjectModel } from '@nestjs/sequelize';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { User } from "./users.model";
+import { InjectModel } from "@nestjs/sequelize";
 
 @Injectable()
 export class UsersService {
@@ -9,9 +9,13 @@ export class UsersService {
     private userModel: typeof User,
   ) {}
 
+  async findAll(): Promise<User[]> {
+    return this.userModel.findAll();
+  }
+
   async remove(id: number): Promise<void> {
     const user = await this.userModel.findByPk(id);
-    if (!user) throw new NotFoundException('User not found');
+    if (!user) throw new NotFoundException("User not found");
     return user.destroy();
   }
 }
